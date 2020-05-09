4820ab895ca41a5aa11e71d8d0749da2b2781406
Nelson Chan <chakflying@hotmail.com>
2020-05-09 18:29:07 +0800

    Add purge css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@
  ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
  */
 module.exports = {
+  purge: ['./**/*.vue'],
   theme: {},
   variants: { textColor: ['responsive', 'hover', 'focus', 'active'] },
   plugins: []
